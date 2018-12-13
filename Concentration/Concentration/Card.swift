fb878b05cd67be604cd2030f69edab4c31e9169d
AJ Caldwell <tomatrow@gmail.com>
2018-12-13 14:03:57 -0800

    Stub out model

---
+++ Concentration/Concentration/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by AJ Caldwell on 12/13/18.
+//  Copyright © 2018 optional(default). All rights reserved.
+//
+
+import Foundation
---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,10 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by AJ Caldwell on 12/13/18.
+//  Copyright © 2018 optional(default). All rights reserved.
+//
+
+import Foundation
+
